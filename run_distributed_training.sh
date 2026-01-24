b8f6b432de28b68d5979583ea56dc445c242bb80
Aurele Gnanha <136912485+Tohokantche@users.noreply.github.com>
2026-01-25 01:06:05 +0800

    Update run_distributed_training.sh

--- run_distributed_training.sh
+++ run_distributed_training.sh
@@ -4,5 +4,5 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 CONFIG_FILE="default_config.yaml"
 N_GPUS=3
 
-torchrun --nproc_per_node $N_GPUS scripts.main \
+torchrun --nproc_per_node $N_GPUS scripts.e2e_training \
     --config  "$SCRIPT_DIR/configs/$CONFIG_FILE"
