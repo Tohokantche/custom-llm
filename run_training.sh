c3e93b5d8404d2c9a538ec7176f906874c01611e
Aurele Gnanha <136912485+Tohokantche@users.noreply.github.com>
2026-01-25 01:05:42 +0800

    Update run_training.sh

--- run_training.sh
+++ run_training.sh
@@ -3,5 +3,5 @@
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 CONFIG_FILE="default_config.yaml"
 
-python -m scripts.train_backup\
+python -m scripts.e2e_training\
     --config  "$SCRIPT_DIR/configs/$CONFIG_FILE"
